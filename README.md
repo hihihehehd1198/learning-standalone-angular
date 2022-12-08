ed19213389d09326c4ad51e932e8eb4ecd73a38e
hihihehehd1198 <50143161+hihihehehd1198@users.noreply.github.com>
2022-12-08 10:00:11 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# learning-standalone-angular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-hsqnau)
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import './polyfills';
+
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+
+// platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
+//   // Ensure Angular destroys itself on hot reloads.
+//   if (window['ngRef']) {
+//     window['ngRef'].destroy();
+//   }
+//   window['ngRef'] = ref;
+
+//   // Otherwise, log the boot error
+// }).catch(err => console.error(err));
+
+bootstrapApplication(AppComponent);
